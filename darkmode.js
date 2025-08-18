e480fe6be1e3269712a01adaf53c082db40b590d
Richard Rhee <rdrhee@gmail.com>
2025-08-17 19:53:29 -0700

    Add sun and moon svg icons

--- darkmode.js
+++ darkmode.js
@@ -1,36 +1,6 @@
-// function setTheme() {
-//     const root = document.documentElement;
-//     const newTheme = root.className === 'dark' ? 'light' : 'dark';
-//     root.className = newTheme;
+var darkmode_icon = document.querySelector("#toggle-icon");
 
-//     document.querySelector('.theme-name').textContent = newTheme;
-// }
-
-// document.querySelector('.theme-toggle').addEventListener('click', setTheme)
-
-const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-
-function handleDarkModeChange(event) {
-  if (event.matches) {
-    // Dark mode is enabled
-    console.log('Dark mode is enabled');
-    // Add your dark mode specific logic here, e.g., add a class to the body
-    document.body.classList.add('dark-mode');
-  } else {
-    // Dark mode is disabled (or not supported)
-    console.log('Dark mode is disabled');
-    // Add your light mode specific logic here, e.g., remove the dark mode class
-    document.body.classList.remove('dark-mode');
-  }
-}
-
-// Initial check
-handleDarkModeChange(darkModeMediaQuery);
-
-// Listen for changes
-darkModeMediaQuery.addEventListener('change', handleDarkModeChange);
-
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle("light");
+function toggle_theme() {
+  var body = document.body;
+  body.classList.toggle("light");
 }
